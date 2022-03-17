f673287936981e21813d03310ebac4065b279e0f
wjc <626063626@qq.com>
2022-03-17 13:11:12 +0800

    first commit

---
+++ modulea/src/main/java/com/bxfox/modulea/ModuleAActivity.java
@@ -0,0 +1,14 @@
+package com.bxfox.modulea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModuleAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_aactivity);
+    }
+}
